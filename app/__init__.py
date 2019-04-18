8ff19da621c448bcbb14b20aa15474bee4a89dc2
kuliye <1440894632@qq.com>
2019-04-18 17:31:50 +0800

    no message

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 import os
 
-
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:xieliang@127.0.0.1:3306/ssmusic"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
@@ -11,19 +10,16 @@ app.config["UP_DIR"] = os.path.join(os.path.abspath(os.path.dirname(__file__)), 
 db = SQLAlchemy(app)
 app.debug = True
 app.template_folder = '../templates'
-app.template_folder = '../static'
+app.static_folder = '../static'
 
 app.config['SECRET_KEY'] = '454'
 
-
-
-
-
-from app.home import home as home_blueprint
+from app.index import index as index_blueprint
 from app.user import user as user_blueprint
 
-app.register_blueprint(home_blueprint)
+app.register_blueprint(index_blueprint)
 app.register_blueprint(user_blueprint)
+
 
 @app.errorhandler(404)
 def page_not_found(error):
--- app/home/__init__.py
+++
@@ -1,3 +0,0 @@
-from flask import Blueprint
-
-home = Blueprint("home", __name__, url_prefix='/home')
