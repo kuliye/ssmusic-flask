924b77741751e1fb8537f1189ba4c177c7c95ca2
kuliye <1440894632@qq.com>
2019-05-09 18:03:58 +0800

    day5

--- app/user/user_view.py
+++ app/user/user_view.py
@@ -7,6 +7,7 @@ from . import user
 from .user_api import UserAPI
 
 
+
 @user.route('/register/', methods=['POST', 'GET'])
 def register():
     if request.method == 'GET':
@@ -15,9 +16,11 @@ def register():
         register_return= UserAPI().register()
         if register_return['status'] != 'success':
             flash('注册失败{0}'.format(register_return['message']), 'error')
+            return redirect(url_for('user.register'))
         else:
             flash('注册成功', 'success')
-        return redirect(url_for('index'))
+            return redirect(url_for('user.login_page'))
+
     else:
         print('无效的方式')
 
@@ -25,6 +28,8 @@ def register():
 @user.route('/login/', methods=['POST', 'GET'])
 def login():
     if request.method == 'GET':
+        return render_template('user/user_login.html')
+    if request.method == 'POST':
         login_return = UserAPI().login()
         if login_return['status'] != 'success':
             flash('登陆失败{0}'.format(login_return['message']), 'error')
@@ -32,7 +37,3 @@ def login():
             flash('登陆成功', 'success')
         return render_template('index.html')
 
-@user.route('/login_page/', methods=['POST', 'GET'])
-def login_page():
-    if request.method == 'GET':
-        return render_template('user/user_login.html')
